4e1b8065c77bf9f58081e70315323d4e6248bf93
zadorotskas <zadorotskas@yandex.ru>
2022-06-26 19:57:39 +0300

    java_ast: change dot generation

--- server/java/java_ast.py
+++ server/java/java_ast.py
@@ -21,18 +21,69 @@ def get_ast(code):
     parser.addErrorListener(my_listener)
     tree = parser.compilationUnit()
 
-    str_tree = Trees.toStringTree(tree, None, parser)
-
-    sub = replace(r'\s\(\s*\)', str_tree, '')
-    sub = replace(r'\s\(\s', sub, '')
-    sub = replace(r'\"', sub, '\\"')
-
-    return get_dot(sub)
+    my_tree = Ast()
+    my_tree.gen_ast(tree, False, 0)
+    my_tree.create_dot()
+    res = my_tree.dot_tree
+    if res == "digraph g {" or res == "digraph g {\n}":
+        raise RuntimeError('Something wrong')
+    return res
 
 
 def replace(regex, string, replacement):
     reg = re.compile(regex)
     return reg.sub(replacement, string)
+
+
+class Ast:
+    def __init__(self):
+        self.line_num = []
+        self.type_rule = []
+        self.content = []
+        self.dot_tree = "digraph g {"
+
+    def gen_ast(self, ctx, verbose, index):
+        ignored = not verbose and ctx.getChildCount() == 1 and isinstance(ctx.getChild(0),
+                                                                          ParserRuleContext)
+        if not ignored:
+            rule_name = Java8Parser.ruleNames[ctx.getRuleIndex()]
+            self.line_num.append(str(index))
+            self.type_rule.append(rule_name)
+            # self.content.append(ctx.getText())  # full text of program
+            self.content.append(self.get_text(ctx))  # text of last nodes
+
+        for i in range(ctx.getChildCount()):
+            element = ctx.getChild(i)
+            if isinstance(element, RuleContext):
+                if ignored:
+                    self.gen_ast(element, verbose, index)
+                else:
+                    self.gen_ast(element, verbose, index + 1)
+
+    def create_dot(self):
+        self.print_label()
+        for i in range(1, len(self.line_num)):
+            pos = self.get_pos(int(self.line_num[i]) - 1, i)
+            self.dot_tree += "\n" + str(int(self.line_num[i]) - 1) + str(pos) + "->" + self.line_num[i] + str(i)
+        self.dot_tree += "\n}"
+
+    def print_label(self):
+        for i in range(len(self.line_num)):
+            self.dot_tree += "\n" + self.line_num[i] + str(i) + "[label=\"" + self.type_rule[i] + "\\n " + self.content[
+                i].replace('"', '\\"') + " \"]"
+
+    def get_pos(self, n, limit):
+        pos = 0
+        for i in range(limit):
+            if int(self.line_num[i]) == n:
+                pos = i
+        return pos
+
+    def get_text(self, node):
+        if node.getChildCount() <= 1:
+            return node.getText()
+        else:
+            return ""
 
 
 class MyErrorListener(ErrorListener):
