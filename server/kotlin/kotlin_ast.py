f6f0c31b02e055abcf30c433a23ac8eca5a777f5
kechin.vv <kechinv1@rambler.ru>
2022-07-05 00:53:15 +0400

    ast class refactoring

--- server/kotlin/kotlin_ast.py
+++ server/kotlin/kotlin_ast.py
@@ -1,8 +1,8 @@
 from antlr4 import *
-from antlr4.error.ErrorListener import ErrorListener
-
 from kotlin.antlr.KotlinLexer import KotlinLexer
 from kotlin.antlr.KotlinParser import KotlinParser
+
+from ast_view.ast import Ast, MyErrorListener
 
 
 def get_ast(code):
@@ -22,7 +22,7 @@ def get_ast(code):
     # sub = str_tree.replace('"', '\\"')
     # print(sub)
     # return get_dot(sub)
-    my_tree = Ast()
+    my_tree = Ast(KotlinParser)
     my_tree.gen_ast(tree, False, 0)
     my_tree.create_dot()
     res = my_tree.dot_tree
@@ -31,61 +31,4 @@ def get_ast(code):
     return res
 
 
-class Ast:
-    def __init__(self):
-        self.line_num = []
-        self.type_rule = []
-        self.content = []
-        self.dot_tree = "digraph g {"
 
-    def gen_ast(self, ctx, verbose, index):
-        ignored = not verbose and ctx.getChildCount() == 1 and isinstance(ctx.getChild(0),
-                                                                          ParserRuleContext)
-        if not ignored:
-            rule_name = KotlinParser.ruleNames[ctx.getRuleIndex()]
-            self.line_num.append(str(index))
-            self.type_rule.append(rule_name)
-            # self.content.append(ctx.getText())  # full text of program
-            self.content.append(self.get_text(ctx))  # text of last nodes
-
-        for i in range(ctx.getChildCount()):
-            element = ctx.getChild(i)
-            if isinstance(element, RuleContext):
-                if ignored:
-                    self.gen_ast(element, verbose, index)
-                else:
-                    self.gen_ast(element, verbose, index + 1)
-
-    def create_dot(self):
-        self.print_label()
-        for i in range(1, len(self.line_num)):
-            pos = self.get_pos(int(self.line_num[i]) - 1, i)
-            self.dot_tree += "\n" + str(int(self.line_num[i]) - 1) + str(pos) + "->" + self.line_num[i] + str(i)
-        self.dot_tree += "\n}"
-
-    def print_label(self):
-        for i in range(len(self.line_num)):
-            self.dot_tree += "\n" + self.line_num[i] + str(i) + "[label=\"" + self.type_rule[i] + "\\n " + self.content[
-                i].replace('"', '\\"') + " \"]"
-
-    def get_pos(self, n, limit):
-        pos = 0
-        for i in range(limit):
-            if int(self.line_num[i]) == n:
-                pos = i
-        return pos
-
-    def get_text(self, node):
-        if node.getChildCount() <= 1:
-            return node.getText()
-        else:
-            return ""
-
-
-class MyErrorListener(ErrorListener):
-
-    def __init__(self):
-        super(MyErrorListener, self).__init__()
-
-    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        raise SyntaxError("line: {}, column: {}, msg: {}, e: {}".format(line, column, msg, e))
