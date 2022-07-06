84f5f73bbdad1e9eca5dcdd6b31fe3e33006399b
semwai <e14s@mail.ru>
2022-07-06 22:27:23 +0300

    update Code model

--- server/models.py
+++ server/models.py
@@ -7,9 +7,10 @@ Base = declarative_base()
 class Code(Base):
     __tablename__ = 'Code'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    description = Column(String(250), nullable=False)
     language = Column(String(50), nullable=False)
     code = Column(Text, nullable=False)
     user_id = Column(Integer, nullable=False)
 
     def __repr__(self):
-        return "<Code(%d, '%s','%s', %d)>" % (self.id, self.language, self.code, self.user_id)
+        return "<Code(%d, '%s', '%s','%s', %d)>" % (self.id, self.description, self.language, self.code, self.user_id)
